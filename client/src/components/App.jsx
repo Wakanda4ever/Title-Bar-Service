9e50d6eafcdbe59948c8cb79fba628cc7fce650f
lhmun <louis.mun@gmail.com>
2018-03-02 20:32:23 -0800

    Removed console log

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -33,8 +33,6 @@ class App extends React.Component {
 		}).catch(function (error) {
 			console.log(error);
 		})
-		console.log('Here is a list of working restaurants: cTJjTKz2huGZ-ElScC2pSw, j37Z4LIXTH9j6KOq9aX8DQ, 1K4qrnfyzKzGgJPBEcJaNQ, NpozCEWra9PkYtxCtCPr6Q, KWdRUyN6H5NIOiP37I9psg, rp0bKQBtAR9XHeeR4CMCOA');
-		console.log('Check database for more');
 	}
 
 	render() {
