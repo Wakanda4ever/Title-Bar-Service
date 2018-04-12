db92b982933b540c261243bed004f373df3ebc32
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-04-11 20:36:59 -0700

    more connectio pooling

--- database/models/cassandra.js
+++ database/models/cassandra.js
@@ -11,7 +11,7 @@ var client = new cassandra.Client({
   ],
   pooling: {
     coreConnectionsPerHost: {
-      [distance.local]: 10,
+      [distance.local]: 100,
       [distance.remote]: 1
     }
   },
