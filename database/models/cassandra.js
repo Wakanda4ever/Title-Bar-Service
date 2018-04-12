7ae1d6457c79e3e9f5a71acdb41a06c2516a2a53
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-04-11 20:24:07 -0700

    added connection pooling

--- database/models/cassandra.js
+++ database/models/cassandra.js
@@ -2,12 +2,19 @@ var databaseHost = '172.31.23.82';
 
 var Promise = require('bluebird');
 var cassandra = require('cassandra-driver');
+var distance = cassandra.types.distance;
 var client = new cassandra.Client({
   contactPoints: [
     '172.31.23.82',
     '172.31.21.142',
     '172.31.27.194'
-   ],
+  ],
+  pooling: {
+    coreConnectionsPerHost: {
+      [distance.local]: 10,
+      [distance.remote]: 1
+    }
+  },
   keyspace: 'chompy_ks'
 });
 
